4d74b6b658e0740c29bb8d496949abfab200d500
Andres Montano <amontano@virginia.edu>
2012-02-29 17:54:13 -0600

    making active resource expire more quickly

--- lib/active_resource/patches/active_resource_patch.rb
+++ lib/active_resource/patches/active_resource_patch.rb
@@ -5,7 +5,7 @@ module ActiveResource
       def find(*arguments)
         begin
           cache_key = ([get_url, element_name]+arguments.reject(&:nil?).collect{|arg| arg.to_s.strip}).reject(&:blank?).join('-')
-          return Rails.cache.fetch(cache_key, :expires_in => 1.day) { old_find(*arguments) }
+          return Rails.cache.fetch(cache_key, :expires_in => 1.hour) { old_find(*arguments) }
         rescue ActiveResource::TimeoutError, ActiveResource::ResourceNotFound, ActiveResource::ServerError
           return nil
         end
