39a6362bf5f873355945bc5cb42d7c8cf0cf7e62
Andres Montano <amontano@virginia.edu>
2012-04-21 00:27:15 -0600

    updating to rails 3.0.x

--- init.rb
+++ init.rb
@@ -2,6 +2,6 @@
 ActiveResource::Base.send :include, ActiveResource::Acts::Tree
 ActiveResource::Base.send :include, ActiveResource::Extensions::URL
 ActiveResource::Base.send :include, ActiveResource::Extensions::Translation
-require 'hpricot'
+# Using hpricot. require 'hpricot' no longer needed as this is done automatically by rails 3 in Gemfile.
 require 'active_resource/patches/traverse'
 require 'active_resource/patches/active_resource_patch'
