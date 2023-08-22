31855ac77b6a408e7f10ae660645105c6cfb9e0c
Andres Montano <amontano@virginia.edu>
2023-08-22 16:26:10 -0600

    updating to rails 6.1

--- active_resource_extensions.gemspec
+++ active_resource_extensions.gemspec
@@ -17,6 +17,6 @@ Gem::Specification.new do |s|
   s.test_files = Dir["test/**/*"]
 
   s.add_dependency 'activeresource'
-  s.add_dependency 'rails', '>= 4.0'
+  s.add_dependency 'rails'#, '>= 4.0'
   s.add_dependency 'hpricot' #, '>= 0.8.6'
 end
--- lib/active_resource_extensions.rb
+++ lib/active_resource_extensions.rb
@@ -1,16 +1,18 @@
 # ActiveResourceExtensions
 # Include hook code here
-require 'active_resource_extensions/acts/tree'
-require 'active_resource_extensions/acts/family_tree'
-require 'active_resource_extensions/extensions/translation'
-require 'active_resource_extensions/extensions/url'
-ActiveResource::Base.send :include, ActiveResourceExtensions::Acts::Tree
-ActiveResource::Base.send :include, ActiveResourceExtensions::Acts::FamilyTree
-ActiveResource::Base.send :include, ActiveResourceExtensions::Extensions::URL
-ActiveResource::Base.send :include, ActiveResourceExtensions::Extensions::Translation
 require 'active_resource_extensions/patches/active_resource_patch'
 require 'active_resource_extensions/patches/http_wrapper'
 require 'active_resource_extensions/patches/connection'
 
+ActiveSupport.on_load(:active_resource) do
+  require 'active_resource_extensions/acts/tree'
+  require 'active_resource_extensions/acts/family_tree'
+  require 'active_resource_extensions/extensions/translation'
+  require 'active_resource_extensions/extensions/url'
+  include ActiveResourceExtensions::Acts::Tree
+  include ActiveResourceExtensions::Acts::FamilyTree
+  include ActiveResourceExtensions::Extensions::Translation
+  include ActiveResourceExtensions::Extensions::URL
+end
 module ActiveResourceExtensions
 end
